f857ce4d37c8c199de893a7404a1a079e13ed1ad
Anton <mordbergak@gmail.com>
2015-02-23 00:58:18 +0300

    Updated resources and manifest

---
+++ app/src/main/java/ru/dinter/antonpp/magiccave/MainApplication.java
@@ -0,0 +1,14 @@
+package ru.dinter.antonpp.magiccave;
+
+import android.app.Application;
+
+/**
+ * Created by Anton on 23.02.2015.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
